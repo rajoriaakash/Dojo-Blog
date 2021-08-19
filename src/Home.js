004b2f1a25a0555199b5b12b79632ac090d21728
rajoriaakash <akashrajoria250103@gmail.com>
2021-08-19 16:25:55 +0530

    Blog list mapping

--- src/Home.js
+++ src/Home.js
@@ -1,20 +1,22 @@
-const Home = () => {
+import { useState } from "react";
 
-    const handleClick =() => {
-        console.log('Hello Ninjas')
-    }
-    const handleClickAgain = (name) =>{
-        console.log('Hello ' + name)
-    }
-    return ( 
-        <div className="Home">
-            <h2>Homepage</h2>
-            <button onClick={handleClick}>Click Me ;)</button>
-            <button onClick={
-                () =>{handleClickAgain('mario')}
-            }>Click me Again</button>
+const Home = () => {
+  const [blogs, setBlogs] = useState([
+    { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+  ])
+
+  return (
+    <div className="home">
+      {blogs.map(blog => (
+        <div className="blog-preview" key={blog.id} >
+          <h2>{ blog.title }</h2>
+          <p>Written by { blog.author }</p>
         </div>
-     );
+      ))}
+    </div>
+  );
 }
  
-export default Home
+export default Home;

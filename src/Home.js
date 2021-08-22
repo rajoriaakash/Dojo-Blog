7216ad1f66ff7aafcb571166a6f8c6d8cd07ec89
rajoriaakash <akashrajoria250103@gmail.com>
2021-08-22 15:08:41 +0530

    handle error, make custom hook

--- src/Home.js
+++ src/Home.js
@@ -1,28 +1,13 @@
-import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
+import useFetch from "./useFetch";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState(null)
-
-
-  const handleDelete = (id) =>{
-    const newBlogs = blogs.filter(blog => blog.id!= id)
-    setBlogs(newBlogs)
-  }
-
-  useEffect(()=>{
-    fetch('http://localhost:8000/blogs')
-      .then(res => {
-        return res.json()
-      })
-      .then(data =>{
-        console.log(data)
-      })
-  })
-
+  const {data : blogs, isPending, error} = useFetch('http://localhost:8000/blogs')
   return (
     <div className="home">
-     {/* <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete}/> */}
+      { error && <div>{error}</div>}
+      {isPending && <div> Loading.. </div>}
+      {blogs && <BlogList blogs={blogs} title="All Blogs"/>}
     </div>
   );
 }

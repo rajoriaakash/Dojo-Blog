4cfbbcb6fa4fae5d8a6047ea3885184ccda6579f
rajoriaakash <akashrajoria250103@gmail.com>
2021-08-26 22:30:25 +0530

    add Create Blog page and post request

--- src/Create.js
+++ src/Create.js
@@ -1,7 +1,54 @@
+import { useState } from "react";
+
 const Create = () => {
+
+    const [title, setTitile] = useState('');
+    const [body, setBody] = useState('');
+    const [author, setAuthor] = useState('mario');
+    const [isPending, setIsPending] = useState(false)
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        const blog = {title, body, author}
+        setIsPending(true)
+
+        fetch('http://localhost:8000/blogs',{
+            method: 'POST',
+            headers: {"Content-type":"application/json"},
+            body: JSON.stringify(blog)
+        }).then(()=>{
+            console.log("New Blog added")
+            setIsPending(false)
+        })
+    }
+
     return ( 
-        <div>
+        <div className = "create">
             <h2>Add a new Blog</h2>
+            <form onSubmit={handleSubmit}>
+                <label>Blog Title: </label>
+                <input 
+                    type="text"
+                    required
+                    value= {title}
+                    onChange = {(e)=> setTitile(e.target.value)}
+                />
+                <label>Blog body: </label>
+                <textarea 
+                required
+                value = {body}
+                onChange = {(e)=> setBody(e.target.value)}
+                ></textarea>
+                <label>Blog Author: </label>
+                <select
+                    value = {author}
+                    onChange = {(e)=>setAuthor(e.target.value)}
+                >
+                    <option value="mario">mario</option>
+                    <option value="yoshi">yoshi</option>
+                </select>
+                {!isPending && <button>Add Blog</button>}
+                {isPending && <button disabled>Adding Blog...</button>}
+            </form>
         </div>
      );
 }
--- src/useFetch.js
+++ src/useFetch.js
@@ -5,7 +5,8 @@ const useFetch = (url) => {
     const [isPending, setIsPending] = useState(true)
     const [error, setError] = useState(null)
     useEffect(()=>{
-        fetch(url)
+      const abortCont = new AbortController()
+        fetch(url,{signal: abortCont.signal})
           .then(res => {
             if(!res.ok){
               throw Error("could not fetch data..sorry")
@@ -18,9 +19,15 @@ const useFetch = (url) => {
             setIsPending(false)
           })
           .catch(err =>{
-            setError(err.message)
-            setIsPending(false)
+            if (err.name === 'AbortError'){
+              console.log("fetch aborted")
+            }else{
+              setError(err.message)
+              setIsPending(false)
+            }
           })
+          return ()=> abortCont.abort(); 
+
       },[url])
     return {data,isPending,error} 
 }
